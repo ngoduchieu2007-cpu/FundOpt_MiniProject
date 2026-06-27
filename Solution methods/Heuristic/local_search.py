865364f4ec3a64c0c79fd8b000fe9cba1149d6c8
baonh2710 <bao.nh.2710@aptechlearning.edu.vn>
2026-06-27 07:19:56 +0700

    greedy & local search

--- Solution methods/Heuristic/local_search.py
+++ Solution methods/Heuristic/local_search.py
@@ -1,4 +1,7 @@
-def greedy_algorithm(N, M, K, q, Q, d):
+# Initialize solution by greedy heuristic, then optimize by local search - first-improvement (insert/relocate)
+# Lấy initial solution của greedy algorithm
+# Input
+def local_search_algorithm_new(N, M, K, q, Q, d):
     def distance_based_greedy_algorithm(N, M, K, q, Q, d):
         routes = [[0] for _ in range(K)]
         current_position = [0] * K
@@ -109,9 +112,7 @@ def greedy_algorithm(N, M, K, q, Q, d):
                 routes[k].append(0)
                 current_distance[k] += d[current_position[k]][0]
 
-        result = routes
-        max_distance = max(current_distance)
-        return result, max_distance
+        return routes, current_distance
 
     def score_based_greedy(N, M, K, q, Q, d):
 
@@ -270,11 +271,180 @@ def greedy_algorithm(N, M, K, q, Q, d):
             if routes[k][-1] != 0:
                 routes[k].append(0)
                 current_distance[k] += d[current_position[k]][0]
-        result = routes
-        max_distance = max(current_distance)
-        return result, max_distance
 
-    res1, max_dist1 = distance_based_greedy_algorithm(N, M, K, q, Q, d)
-    res2, max_dist2 = score_based_greedy(N, M, K, q, Q, d)
-    res = res1 if max_dist1 <= max_dist2 else res2
-    return res
+        return routes, current_distance
+
+    res1, dist1 = distance_based_greedy_algorithm(N, M, K, q, Q, d)
+    res2, dist2 = score_based_greedy(N, M, K, q, Q, d)
+    routes, current_distance = (
+        (res1, dist1) if max(dist1) <= max(dist2) else (res2, dist2)
+    )
+
+    # Đã tìm ra initial solution - candidate theo thuật toán greedy
+
+    ## Bắt đầu thuật toán local search: insert thứ tự đi của 1 xe / relocate việc giữa 2 xe
+
+    ### hàm kiểm tra tính hợp lệ của neighbour solution
+    def check_valid_and_cost(k, test_route):
+        load = 0
+        cost = 0
+        visited = [0] * (2 * N + 2 * M + 1)
+        for visit_order, location in enumerate(test_route):
+            #### luật 1: đón khách xong phải trả khách luôn
+            if visit_order == 0:
+                continue
+            if location in range(1, N + 1):
+                if test_route[visit_order + 1] != location + N + M:
+                    return False, None
+            #### luật 2: load trên xe không vượt quá Q[k]
+            #### luật 3: phải đến điểm lấy hàng trước khi trả hàng
+            if location in range(1 + N, N + M + 1):
+                visited[location] = 1
+                load += q[location - 1 - N]
+                if load > Q[k]:
+                    return False, None
+                if test_route.index(location + M + N) < visit_order:
+                    return False, None
+            if location in range(2 * N + M + 1, 2 * N + 2 * M + 1):
+                if visited[location - N - M] == 0:
+                    return False, None
+                load -= q[location - 2 * N - M - 1]
+                if test_route.index(location - N - M) > visit_order:
+                    return False, None
+            cost += d[test_route[visit_order - 1]][location]
+        return True, cost
+
+    improve_by_insert = True
+    improve_by_relocate = True
+    while improve_by_insert or improve_by_relocate:
+        improve_by_insert = False
+        improve_by_relocate = False
+        ## cách 1: insert - rút 1 điểm chèn vào thứ tự khác
+        for k in range(K):
+            route = routes[k]
+            _, current_cost = check_valid_and_cost(k, route)
+            for visit_order, location in enumerate(route):
+                if location == 0:
+                    continue
+                if location in range(1, N + 1):
+                    test_route = route.copy()
+                    test_route.remove(location)
+                    test_route.remove(location + N + M)
+                    for insert_index in range(1, len(test_route) - 1):
+                        test_route.insert(insert_index, location)
+                        test_route.insert(insert_index + 1, location + N + M)
+                        test_valid, test_cost = check_valid_and_cost(k, test_route)
+                        if test_valid and test_cost < current_cost:
+                            routes[k] = test_route
+                            current_distance[k] = test_cost
+                            improve_by_insert = True
+                            break
+                    if improve_by_insert:
+                        break
+                elif location in range(N + 1, N + M + 1):
+                    test_route = route.copy()
+                    test_route.remove(location)
+                    for insert_index in range(
+                        1, test_route.index(location + M + N) + 1
+                    ):
+                        test_route.insert(insert_index, location)
+                        test_valid, test_cost = check_valid_and_cost(k, test_route)
+                        if test_valid and test_cost < current_cost:
+                            routes[k] = test_route
+                            current_distance[k] = test_cost
+                            improve_by_insert = True
+                            break
+                    if improve_by_insert:
+                        break
+                elif location in range(2 * N + M + 1, 2 * N + 2 * M + 1):
+                    test_route = route.copy()
+                    test_route.remove(location)
+                    for insert_index in range(
+                        test_route.index(location - N - M) + 1, len(test_route)
+                    ):
+                        test_route.insert(insert_index, location)
+                        test_valid, test_cost = check_valid_and_cost(k, test_route)
+                        if test_valid and test_cost < current_cost:
+                            routes[k] = test_route
+                            current_distance[k] = test_cost
+                            improve_by_insert = True
+                            break
+                    if improve_by_insert:
+                        break
+
+        ## cách 2: relocate nhiệm vụ giữa xe đang có distance cao nhất giao cho xe thấp nhất
+        current_max_distance = max(current_distance)
+        taxi_with_highest_distance = current_distance.index(current_max_distance)
+        taxi_with_lowest_distance = current_distance.index(min(current_distance))
+        route_1 = routes[taxi_with_highest_distance]
+        route_2 = routes[taxi_with_lowest_distance]
+
+        for visit_order_1, location_1 in enumerate(route_1):
+            ### giao việc đón trả khách
+            if location_1 in range(1, N + 1):
+                for insert_index in range(1, len(route_2)):
+                    test_route_1 = route_1.copy()
+                    test_route_2 = route_2.copy()
+                    test_route_2.insert(insert_index, location_1)
+                    test_route_2.insert(insert_index + 1, location_1 + N + M)
+                    test_route_1.remove(location_1)
+                    test_route_1.remove(location_1 + N + M)
+                    route_1_is_valid, route_1_cost = check_valid_and_cost(
+                        taxi_with_highest_distance, test_route_1
+                    )
+                    route_2_is_valid, route_2_cost = check_valid_and_cost(
+                        taxi_with_lowest_distance, test_route_2
+                    )
+                    if (
+                        route_1_is_valid
+                        and route_2_is_valid
+                        and max(route_1_cost, route_2_cost) < current_max_distance
+                    ):
+                        improve_by_relocate = True
+                        routes[taxi_with_highest_distance] = test_route_1
+                        current_distance[taxi_with_highest_distance] = route_1_cost
+                        routes[taxi_with_lowest_distance] = test_route_2
+                        current_distance[taxi_with_lowest_distance] = route_2_cost
+                        break
+                if improve_by_relocate:
+                    break
+
+            ### giao việc nhận và trả hàng
+            elif location_1 in range(1 + N, 1 + N + M):
+                for insert_index_pick in range(1, len(route_2)):
+                    if route_2[insert_index_pick - 1] in range(1, N + 1):
+                        continue
+                    for insert_index_drop in range(
+                        insert_index_pick + 1, len(route_2) + 1
+                    ):
+                        if route_2[insert_index_drop - 1] in range(1, N + 1):
+                            continue
+                        test_route_1 = route_1.copy()
+                        test_route_2 = route_2.copy()
+                        test_route_2.insert(insert_index_pick, location_1)
+                        test_route_2.insert(insert_index_drop, location_1 + M + N)
+                        test_route_1.remove(location_1)
+                        test_route_1.remove(location_1 + N + M)
+                        route_1_is_valid, route_1_cost = check_valid_and_cost(
+                            taxi_with_highest_distance, test_route_1
+                        )
+                        route_2_is_valid, route_2_cost = check_valid_and_cost(
+                            taxi_with_lowest_distance, test_route_2
+                        )
+                        if (
+                            route_1_is_valid
+                            and route_2_is_valid
+                            and max(route_1_cost, route_2_cost) < current_max_distance
+                        ):
+                            improve_by_relocate = True
+                            routes[taxi_with_highest_distance] = test_route_1
+                            routes[taxi_with_lowest_distance] = test_route_2
+                            current_distance[taxi_with_highest_distance] = route_1_cost
+                            current_distance[taxi_with_lowest_distance] = route_2_cost
+                            break
+                    if improve_by_relocate:
+                        break
+                if improve_by_relocate:
+                    break
+
+    return routes
